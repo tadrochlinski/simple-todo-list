6630540b53ade8c4456953477e09ebfe0e6b09f3
tadrochlinski <tadrochlinski@gmail.com>
2022-07-04 22:54:07 +0200

    deleting todos done

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const todoElement = (content) =>{
             ${content}
         </div>
         <div class="todo__buttons">
-            <button class="todo__completed-btn">
+            <button class="todo__check-btn">
                 <i class="fa-solid fa-check check"></i>
             </button>
             <button class="todo__delete-btn">
@@ -28,6 +28,10 @@ window.onload = () =>{
     if(localStorage.length > 0){
         //refresh todo output
         refreshOutput();
+
+        todos.forEach(element =>{
+            console.log(element);
+        });
     }
 }
 
@@ -50,13 +54,48 @@ todoForm.addEventListener('submit', (e) =>{
     }
 });
 
+//when todo list element is clicked
+todoListDiv.addEventListener('click', (e) =>{
+    //saving clicked element into variable
+    const clickedElement = e.target;
+    //checking if clicked element is delete todo button
+    if(clickedElement.classList[0] === 'todo__delete-btn'){
+        //searching for todo div to delete
+        const todoDiv = clickedElement.closest('li');
+        //removing todo 
+        removeTodo(todoDiv);
+    }
+});
+
 //functions
+const removeTodo = (todoDiv) =>{
+    //saving todo div id into variable
+    const todoDivId = todoDiv.id;
+    
+    //searching through todos array maching array element 
+    todos.forEach((todo, index) => {
+        //saving id from todo object into a variable
+        const todoId = todo.id;
+        
+        //checking if todo object from an array id is equal to todo div id
+        if(todoId == todoDivId){
+            //deleting todo from tab
+            todos.splice(index, 1);
+            //saving changed todos tab in localStorage
+            localStorage.setItem('todos', JSON.stringify(todos));
+            //refreshing todo list output div
+            refreshOutput();
+        }
+    })
+}
+
 const refreshOutput = () =>{
-    //cleaning output 
+
+    //cleaning output
     todoListDiv.innerHTML = '';
     //uptading todos tab
     todos = JSON.parse(localStorage.getItem('todos'));
-    //handling empty state div 
+    //handling empty state div
     (todos.length > 0) ? emptyStateDiv.style.display = 'none' : emptyStateDiv.style.display = 'block';
     //creating todo divs in output
     todos.forEach(todo =>{
@@ -75,10 +114,10 @@ const createTodoDiv = (id, content, checked) =>{
     todo.classList.add('todo');
     //adding checked class if todo is checked
     if(checked){
-        todo.classList.add('todo--checked'); 
-        const completedBtn = todo.querySelector('.todo__completed-btn');
-        completedBtn.classList.add('todo__completed-btn--checked');
+        todo.classList.add('todo--checked');
+        const completedBtn = todo.querySelector('.todo__check-btn');
+        completedBtn.classList.add('todo__check-btn--checked');
     }
-    //append todo li to list div 
+    //append todo li to list div
     todoListDiv.appendChild(todo);
 }
